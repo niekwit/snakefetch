bd531e8fe2dc1455e519ff80aa45513fddf4a62d
niekwit <41302859+niekwit@users.noreply.github.com>
2024-04-23 14:17:27 +0100

    can fetch directories other than config,workflow now

--- src/snakefetch/snakefetch.py
+++ src/snakefetch/snakefetch.py
@@ -1,60 +1,95 @@
 import argparse
 import tempfile
 import re
-from urllib.request import urlretrieve
 import subprocess
-import sys
 import os
+import sys
+from urllib.request import urlretrieve
 
-def fetch_repo(url, version, dir):
+VERSION = "0.5.0"
+
+def fetch_repo(url, version, out_dir, target_dirs):
     """
-    This will download the tar.gz file of the repository and extract it to the specified directory.
+    Download the tar.gz file of given repository and extract it to the specified directory.
     It will only untar the config and workflow directories.
-    """
-    # Check if version starts with 'v'
-    # If so, remove it (is not included in the URL somehow)
-    if re.match(r"^v", version):
-        version = version[1:]
-        
-    target_url = f"{url}/archive/refs/tags/{version}.tar.gz"
-    repo_name = url.split("/")[-1]
-    
+    """        
     # Download the tar.gz file in temp dir and untar to specified directory
     with tempfile.TemporaryDirectory() as tmpdirname:
-        print("Downloading tar.gz file...")
+        target_url = f"{url}/archive/refs/tags/{version}.tar.gz"
+        print(f"Downloading archive file for version {version} from {url}...")
         download_file = os.path.join(tmpdirname, "target_repo.tar.gz")
         urlretrieve(target_url, download_file)
         
-        print("Extracting config and workflow directories from tar.gz file...")
-        subprocess.run([
-            "tar", "-xzf", download_file, 
-            "--wildcards", f"{repo_name}/config/*", f"{repo_name}/workflow/*",
-            "--strip-components=1", f"--one-top-level={dir}"
-            ])
+        print(f"Extracting config and workflow directories from tar.gz file to {out_dir}...")
         
-        print("Done!")
-    
+        # Prepare repo name
+        # Check if version starts with 'v'
+        # If so, remove it (is not included in the archive somehow)
+        if re.match(r"^v", version):
+            version = version[1:]
+            
+        repo_name = url.split("/")[-1]
+        repo_name = f"{repo_name}-{version}"
+        
+        # Prepare target directories command line argument(s)
+        wildcards = []
+        for d in target_dirs.split(","):
+            wildcards.append(f"{repo_name}/{d}/*")
+        
+        # Construct tar command
+        tar = ["tar", "-xzf", download_file, "--wildcards"]
+        tar.extend(wildcards)
+        tar.extend(["--strip-components=1", f"--one-top-level={out_dir}"])
+
+        # Untar archive and extract only the config and workflow directories
+        exit_code = subprocess.run(tar)
+        
+        if exit_code.returncode == 0:
+            print("Done!")
+            return True
+        else:
+            raise Exception(f"Failed to extract {target_dirs} directories from tar.gz file to {out_dir}")
 
 if __name__ == "__main__":
+    # Command line parser
     parser = argparse.ArgumentParser(prog = "snakefetch",
-                                     description = "Fetch config and workflow files from a Snakemake repository on GitHub",)
+                                     description = "Fetch config and workflow directories from a versioned Snakemake repository on GitHub",)
     parser.add_argument(
-        "--dir", "-d",
-        type=str,
-        default="./",
-        help="Directory to save the files to",
+        "--outdir", "-o",
+        type = str,
+        default = "./",
+        help = "Directory to save the files to",
     )
     parser.add_argument(
         "--url", "-u",
-        type=str,
-        help="URL of the Snakemake repository on GitHub",
+        type = str,
+        required = "--version" not in sys.argv,
+        help = "URL of the Snakemake repository on GitHub",
     )
     parser.add_argument(
-        "--version", "-v",
-        type=str,
-        help="Release version of the Snakemake repository on GitHub",
+        "--repo-version", "-v",
+        dest = "repoversion",
+        type = str,
+        required = "--version" not in sys.argv,
+        help = "Release version of the Snakemake repository on GitHub",
     )
-    
+    parser.add_argument(
+        "--target-dirs", "-t",
+        dest = "targetdirs",
+        type = str,
+        default = "config,workflow",
+        help = "Directories to fetch from the Snakemake repository on GitHub (default: %(default)s)",
+    )
+    parser.add_argument(
+        "--version",
+        action = "store_true",
+        help = "Print snakefetch version and exit",
+    )
     args = parser.parse_args()
-
     
+    if args.version:
+        print(f"snakefetch version {VERSION}")
+        exit(0)
+    
+    # Fetch repo directories
+    fetch_repo(args.url, args.repoversion, args.outdir, args.targetdirs)
